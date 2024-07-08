a9d47384cfdac176376b8ecfb8584d336f4775f1
VamsiKrishnaRAVSR <107360297+VamsiKrishnaRAVSR@users.noreply.github.com>
2024-07-08 22:34:35 +0530

    Connected with databases

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const mysql = require("mysql");
 const { v4: uuidv4 } = require("uuid");
 
 const app = express();
@@ -13,41 +14,72 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+var db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "todos"
+});
+
+db.connect(function(err) {
+  if (err) throw err;
+  console.log("connected");
+   //   con.destroy()
+});
+
 app.get("/todos", (req, res) => {
-  res.json(todos);
+  db.query(`select * from todoList;`, (err, result) => {
+    if (err) throw err
+    const todoList  = result.map((ele) => ({
+      id: ele.id,
+      text: ele.item,
+      isCompleted: ele.isCompleted
+    }));
+    res.json(todoList)
+  })
+  // res.json(todos);
 });
 
 app.post("/todos", (req, res) => {
   const todo = req.body;
-  const newTodo = {
-    id: uuidv4(),
-    text: todo.text,
-    isCompleted: todo.isCompleted,
-  };
-  console.log(newTodo);
-  todos.push(newTodo);
-
-  res.send("Todo is added to the database");
+  // const newTodo = {
+  //   text: todo.text,
+  //   isCompleted: todo.isCompleted,
+  // };
+  const query = `insert into todoList(isCompleted, item) values (${todo.isCompleted}, "${todo.text}")`
+  db.query(query, (err, result) => {
+    if(err) {
+      res.json(err.sqlMessage);
+      throw err
+    }
+    res.json("Successfully added into the database")
+  })
 });
 
 app.delete("/todos/:id", (req, res) => {
   const todoId = req.params.id;
-
-  todos = todos.filter((todo) => todo.id !== todoId);
-
-  res.send("Todo is deleted");
+  db.query(`delete from todoList where id=${todoId}`, (err, result) => {
+    if(err) throw err;
+    res.send(`Successfully deleted ${todoId} from the database`);
+  })
 });
 
 app.patch("/todos/:id", (req, res) => {
   const todoId = req.params.id;
-  todos = todos?.map((ele) => {
-    if (ele?.id === todoId) {
-      return { ...ele, isCompleted: !ele?.isCompleted };
+  var todoItem = {}
+  db.query(`select * from todoList where id =${todoId}`, (err, result) => {
+    if (err) {
+      throw err
     }
-    return ele;
-  });
+    todoItem['id'] = result[0].id;
+    todoItem['isCompleted'] = !result[0].isCompleted
+    todoItem['text'] = result[0].item
+    db.query(`update todoList set isCompleted= ${todoItem.isCompleted} where id=${todoId}`, (err, result) => {
+      if(err) throw err
+      res.send("Todo is updated");
+    });
+  })
 
-  res.send("Todo is updated");
 });
 
 app.listen(port, () => console.log(`The app is listening on a port ${port}!`));
